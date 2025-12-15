876e8bc6a371bae0c28971a0841a1e03ad90979e
Denisa <denisanita100@yahoo.com>
2025-12-15 23:05:10 +0200

    Fix indentation in vulnerable.py

--- vulnerable.py
+++ vulnerable.py
@@ -2,5 +2,5 @@ import subprocess
 import os
 
 def run_command():
-cmd = os.environ.get("VULN_CMD")  # User-controlled input
-    subprocess.run(cmd, shell=True)  # This is a vulnerability
+	cmd = os.environ.get("VULN_CMD")  # User-controlled input
+	subprocess.run(cmd, shell=True)  # This is a vulnerability
